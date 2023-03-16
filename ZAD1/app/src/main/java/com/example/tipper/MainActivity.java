324256ad59b19a16c33c104e40fedf81545970f6
s13146-pj <56793702+s13146@users.noreply.github.com>
2023-03-16 21:18:49 +0100

    Add task

---
+++ ZAD1/app/src/main/java/com/example/tipper/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.tipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText; //
+import android.widget.TextView; //
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText weightEdit;
+    private EditText growthEdit;
+    private TextView bmiTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // call superclass onCreate
+        setContentView(R.layout.activity_main); // inflate the GUI
+
+        // get references to programmatically manipulated TextViews
+        weightEdit = (EditText) findViewById(R.id.weightEditText);
+        growthEdit = (EditText) findViewById(R.id.growthEditText);
+        bmiTextView = (TextView) findViewById(R.id.bmiTextView);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                calculate();
+            }
+
+        });
+    }
+    private void calculate() {
+
+        float weight =  Float.parseFloat(weightEdit.getText().toString());
+        float growth = Float.parseFloat(growthEdit.getText().toString()) / 100;
+        float bmi = growth/(weight*weight);
+        String calculatedBmi = String.format("%.2f", bmi);
+        bmiTextView.setText(calculatedBmi);
+
+    }
+
+
+}
+
